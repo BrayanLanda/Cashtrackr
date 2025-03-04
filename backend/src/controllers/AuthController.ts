6bbb0155143de49679d0c38d41b694f28f190cd1
Brayan Acosta <landakkpp@gmail.com>
2025-03-03 21:08:58 -0500

    add router and controller by user, and hast password and create token

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -1,7 +1,24 @@
 import { Request, Response } from "express";
+import User from "../models/User";
+import { hashPassword } from "../utils/auth";
+import { generateToken } from "../utils/token";
 
 export class AuthController {
     static createAccount = async (req: Request, res: Response) => {
-
+        const { email, password } = req.body;
+        try {
+            const userExists = await User.findOne({ where: { email } });
+            if (userExists) {
+                res.status(409).json({ message: 'User already exists' });
+                return;
+            }
+            const user = new User(req.body);
+            user.password = await hashPassword(password);
+            user.token = generateToken();
+            await user.save();
+            res.status(201).json({ message: 'User created successfully' });
+        } catch (error) {
+            res.status(500).json({ error: "There was an error creating the user" });
+        }
     }
 }
